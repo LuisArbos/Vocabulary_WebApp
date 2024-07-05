588a96e8f238896750c07290ae644de3d39599d6
LuisArbos <luisarbos@gmx.com>
2024-07-05 21:40:29 +0200

    Updating home page, first version in progress

--- frontend/public/static/js/scripts.js
+++ frontend/public/static/js/scripts.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.dropdown-toggle').forEach(function (element) {
-        element.addEventListener('click', function (event) {
-            let dropdownMenu = this.nextElementSibling;
-            dropdownMenu.classList.toggle('show');
+document.querySelectorAll('.dropdown-toggle').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        let dropdownMenu = this.nextElementSibling;
+        dropdownMenu.classList.toggle('show');
         });
     });
-});
