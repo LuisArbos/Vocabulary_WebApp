48053a3e933614e424fe84a49f22b504baebe7ad
LuisArbos <luisarbos@gmx.com>
2024-07-05 22:35:29 +0200

    Updating more views, almost ready to begin with the main part

--- backend/vocabulary_webapp/apps/vocabulary/urls.py
+++ backend/vocabulary_webapp/apps/vocabulary/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('practice/', views.practice_view, name='practice'),
-    path('practice-language/<str:language>/', views.practice_language_view, name='practice_language')
+    path('en/practice/', views.en_practice_view, name='en_practice_view'),
+    path('es/practice/', views.es_practice_view, name='es_practice_view'),
+    path('change-practice-language/<str:language>/', views.change_practice_language, name='change_practice_language'),
+    path('en/practice-language/<str:language>/', views.en_practice_language_view, name='en_practice_language'),
+    path('es/practice-language/<str:language>/', views.es_practice_language_view, name='es_practice_language')    
 ]
--- backend/vocabulary_webapp/apps/vocabulary/views.py
+++ backend/vocabulary_webapp/apps/vocabulary/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Language
+from django.utils.translation import activate
 
 # Create your views here.
 def language(request):
     items = Language.objects.all()
     return render(request, "test.html", {"tests": items})
 
-def practice_view(request):
-    print("Practice view requested.")
-    return render(request, 'language_selection.html')
+def en_practice_view(request):
+    context = {
+        'current_template': 'language_selection.html'
+    }
+    return render(request, 'language_selection.html', context)
 
-def practice_language_view(request, language):
+def es_practice_view(request):
+    context = {
+        'current_template': 'language_selection_es.html'
+    }
+    return render(request, 'language_selection_es.html', context)
+
+def change_practice_language(request, language):
+    activate(language)
+    response = redirect('en_practice_view' if language == 'en' else 'es_practice_view')
+    response.set_cookie('django_language', language)
+    return response
+
+def en_practice_language_view(request, language):
     context = {'language': language}
     return render(request, 'practice_language.html', context)
+
+def es_practice_language_view(request, language):
+    context = {'language': language}
+    return render(request, 'practice_language.html', context)
+
